e889780c730cddd6150100c70e607ccdb5731f0d
JOHANN BENJAMIN DURAN TICONA <johann.bdticona@senacsp.edu.br>
2023-03-03 21:07:44 -0300

    Adição de verificador na tela de login

--- MiniProjeto/frmLogin.cs
+++ MiniProjeto/frmLogin.cs
@@ -42,6 +42,26 @@ namespace MiniProjeto
             }
         }
 
+        private bool Verificador()
+        {
+            if (string.IsNullOrEmpty(txtUsuario.Text))
+            {
+                mensagemErro = "Erro! Informe um email";
+                txtUsuario.Text = "";
+                txtUsuario.Focus();
+                return false;
+            }
+
+            if(string.IsNullOrEmpty(txtUsuario.Text))
+            {
+                mensagemErro = "Erro! Informe uma senha";
+                txtSenha.Text = "";
+                txtSenha.Focus();
+                return false;
+            }
+            return true;
+        }
+
         private void frmLogin_Load(object sender, EventArgs e)
         {
             TestarConexao();
@@ -54,41 +74,48 @@ namespace MiniProjeto
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-            string sql = "select nome_usuario, login_usuario, senha_usuario from Usuario where " +
-                "login_usuario = '" + txtUsuario.Text + "' and " +
-                "senha_usuario = '" + txtSenha.Text + "'";
-
-            SqlConnection conn = new SqlConnection(stringConexao);
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            cmd.CommandType = CommandType.Text;
-            SqlDataReader leitura;
-            conn.Open();
-
-            try
+            if (Verificador())
             {
-                leitura = cmd.ExecuteReader();
-                if (leitura.Read())
+                string sql = "select nome_usuario, login_usuario, senha_usuario from Usuario where " +
+                    "login_usuario = '" + txtUsuario.Text + "' and " +
+                    "senha_usuario = '" + txtSenha.Text + "'";
+
+                SqlConnection conn = new SqlConnection(stringConexao);
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader leitura;
+                conn.Open();
+
+                try
                 {
-                    MessageBox.Show("Seja Bem vindo! " + leitura[0].ToString());
-                    MDITelaInicial mdi = new MDITelaInicial();
-                    mdi.Show();
-                    this.Hide();
+                    leitura = cmd.ExecuteReader();
+                    if (leitura.Read())
+                    {
+                        MessageBox.Show("Seja Bem vindo! " + leitura[0].ToString());
+                        MDITelaInicial mdi = new MDITelaInicial();
+                        mdi.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuário ou senha inválidas!");
+                        txtUsuario.Text = "";
+                        txtSenha.Text = "";
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Usuário ou senha inválidas!");
-                    txtUsuario.Text = "";
-                    txtSenha.Text = "";
+                    MessageBox.Show("Erro: " + ex.Message);
+                    Application.Exit();
+                }
+                finally
+                {
+                    conn.Close();
                 }
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show("Erro: " + ex.Message);
-                Application.Exit();
-            }
-            finally
-            {
-                conn.Close();
+                MessageBox.Show(mensagemErro);
             }
         }
     }
